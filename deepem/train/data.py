495829f7230b41166fa33e5751b5dbf7b24e690f
torms3 <kisuklee@mit.edu>
2018-05-19 14:30:12 -0400

    reorganize data

--- deepem/train/data.py
+++ deepem/train/data.py
@@ -2,9 +2,28 @@ from __future__ import print_function
 import imp
 
 import torch
+import torch.utils.data
 from torch.utils.data import DataLoader
 
-from deepem.data.dataset import Dataset, worker_init_fn
+
+def worker_init_fn(worker_id):
+    # Each worker already has its own random state (Torch).
+    seed = torch.IntTensor(1).random_()[0]
+    # print("worker ID = {}, seed = {}".format(worker_id, seed))
+    np.random.seed(seed)
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, sampler, size):
+        super(Dataset, self).__init__()
+        self.sampler = sampler
+        self.size = size
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        return self.sampler()
 
 
 class Data(object):
