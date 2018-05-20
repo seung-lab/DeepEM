011b5a27a3ff579c1fbbe404601399c17ad3ed9c
torms3 <kisuklee@mit.edu>
2018-05-20 17:14:28 -0400

    mirror & crop

--- deepem/test/option.py
+++ deepem/test/option.py
@@ -49,6 +49,7 @@ class Options(object):
         self.parser.add_argument('--out_prefix', default='')
         self.parser.add_argument('--out_tag', default='')
         self.parser.add_argument('--overlap', type=vec3f, default=(0.5,0.5,0.5))
+        self.parser.add_argument('--mirror', type=vec3, default=None)
         self.parser.add_argument('--crop', type=vec3, default=None)
 
         # Benchmark
