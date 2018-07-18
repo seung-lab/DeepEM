ed8fe9d09ed06673cc5a7db90db8c3525f02f6f0
Kisuk Lee <kisuklee@mit.edu>
2018-07-18 13:50:59 -0400

    BlendLoss

---
+++ deepem/loss/blend.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from deepem.loss.edge import EdgeLoss
+from deepem.loss.mean import MeanLoss
+
+
+class BlendLoss(nn.Module):
+    def __init__(self, **kwargs):
+        super(BlendLoss, self).__init__()
+        self.edge = EdgeLoss(**kwargs)
+        self.mean = MeanLoss(**kwargs)
+
+    def forward(self, x, label, mask):
+        if np.random.rand() > 0.5:
+            return self.edge(x, label, mask)
+        else:
+            return self.mean(x, label, mask)
