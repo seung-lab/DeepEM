9a0cbda063613babca7b9f3cb622fb6284680263
torms3 <kisuklee@mit.edu>
2018-07-24 20:00:06 -0400

    vec_aff_mye sampler initial commit

--- deepem/data/sampler/vec_aff_mye.py
+++ deepem/data/sampler/vec_aff_mye.py
@@ -10,6 +10,8 @@ def get_spec(in_spec, out_spec):
         spec[k] = v[-3:]
     for k, v in out_spec.items():
         dim = tuple(v[-3:])
+        if k == 'affinity':
+            dim = tuple(d+2 for d in dim)
         spec[k] = dim
         spec[k+'_mask'] = dim
     return spec
@@ -26,6 +28,7 @@ class Sampler(object):
 
     def postprocess(self, sample):
         assert 'embedding' in sample
+        assert 'affinity' in sample
         assert 'myelin' in sample
         sample = Augment.to_tensor(sample)
         return self.to_float32(sample)
@@ -42,7 +45,7 @@ class Sampler(object):
             dp.add_dataset(self.build_dataset(k, data[k]))
         dp.set_augment(aug)
         dp.set_imgs(['input'])
-        dp.set_segs(['embedding'])
+        dp.set_segs(['embedding','affiity'])
         if prob:
             dp.set_sampling_weights(p=[prob[k] for k in keys])
         else:
@@ -60,6 +63,8 @@ class Sampler(object):
         dset.add_data(key='input', data=img)
         dset.add_data(key='embedding', data=seg)
         dset.add_mask(key='embedding_mask', data=msk, loc=loc)
+        dset.add_data(key='affinity', data=seg)
+        dset.add_data(key='affinity_mask', data=msk)
         dset.add_data(key='myelin', data=mye)
         dset.add_mask(key='myelin_mask', data=msk)
         return dset
