ee60592fc1b170fc7551a2915a02e99a78d7f8c1
Kisuk Lee <kisuklee@mit.edu>
2018-10-05 09:22:29 -0400

    update

--- deepem/data/dataset/SNEMI3D/train75_val25/segmb.py
+++ deepem/data/dataset/SNEMI3D/train75_val25/segmb.py
@@ -10,6 +10,7 @@ data_info = {
     'train':{
         'img': 'img.h5',
         'seg': 'segmb.h5',
+        'bdr': 'segb.h5',
         'msk': 'msk.h5',
         'mye': 'mye.h5',
         'dir': 'train75_val25/mip0/padded_x0_y0_z0/train',
@@ -18,6 +19,7 @@ data_info = {
     'val':{
         'img': 'img.h5',
         'seg': 'segmb.h5',
+        'bdr': 'segb.h5',
         'msk': 'msk.h5',
         'mye': 'mye.h5',
         'dir': 'train75_val25/mip0/padded_x0_y0_z0/val',
@@ -26,18 +28,18 @@ data_info = {
 }
 
 
-def load_data(data_dir, data_ids=None, mye=False, **kwargs):
+def load_data(data_dir, data_ids=None, bdr=False, mye=False, **kwargs):
     if data_ids is None:
         data_ids = data_info.keys()
     data = dict()
     dpath = os.path.expanduser(data_dir)
     for data_id in data_ids:
         info = data_info[data_id]
-        data[data_id] = load_dataset(dpath, data_id, info, mye=mye)
+        data[data_id] = load_dataset(dpath, data_id, info, bdr=bdr, mye=mye)
     return data
 
 
-def load_dataset(dpath, tag, info, mye=False):
+def load_dataset(dpath, tag, info, bdr=False, mye=False):
     dset = dict()
 
     # Image
@@ -52,11 +54,18 @@ def load_dataset(dpath, tag, info, mye=False):
     seg = emio.imread(fpath).astype('uint32')
     dset['seg'] = seg
 
+    # Boundary
+    if bdr:
+        fpath = os.path.join(dpath, info['dir'], info['bdr'])
+        print(fpath)
+        bdr = emio.imread(fpath)
+        dset['bdr'] = (seg != 0).astype('uint8')
+
     # Myelin
     if mye:
         fpath = os.path.join(dpath, info['dir'], info['mye'])
         print(fpath)
-        mye = emio.imread(fpath).astype('float32')
+        mye = emio.imread(fpath).astype('uint8')
         dset['mye'] = mye
 
     # Train mask
