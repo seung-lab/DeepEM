d9b612ede6076382dc6e3afdcdb7ccdb273b7cff
torms3 <kisuklee@mit.edu>
2019-10-04 20:31:18 -0400

    update

--- deepem/data/modifier/crop2x.py
+++ deepem/data/modifier/crop2x.py
@@ -7,7 +7,7 @@ class Modifier(object):
     def __call__(self, sample):
         if np.random.rand() < 0.5:
             return sample
-        for k, v in sample:
+        for k, v in sample.items():
             cropsz = (v.shape[-3], v.shape[-2]//2, v.shape[-1]//2)
             sample[k] = torch_utils.crop_center(v, cropsz)
         return sample
